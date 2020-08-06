b616cadc869b886d68f9ff6730631dd04b2e7979
Jing <jshota@hotmail.com>
2020-08-06 14:08:12 -0400

    updating the map impl. Closes #22

--- pet-clinic-web/src/main/java/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/springpetclinic/bootstrap/DataLoader.java
@@ -24,14 +24,12 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Fiona");
         owner1.setLastName("Sit");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Khil");
         owner2.setLastName("Fong");
 
@@ -40,20 +38,17 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded owners");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Vet1");
         vet1.setLastName("Sit");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Vet2");
         vet2.setLastName("Fong");
 
         vetService.save(vet2);
 
         System.out.println("Loaded vets");
-
     }
 }
